74c4e1c3e3281d05400b3a5792958d922f680a3d
pablolucio97 <pablolucio97@users.noreply.github.com>
2020-12-04 10:04:38 -0300

    Altered some styles

--- css_box_preview/src/App.js
+++ css_box_preview/src/App.js
@@ -1,48 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Divider} from '@material-ui/core'
 
+import './styles/index.css'
+
+
 function App() {
-  return (
+
+  const [width, setWidth] = useState(100)
+  const [height, setHeight] = useState(100)
+  const [rotation, setRotation] = useState(20)
+  const [border, setBorder] = useState (0)
+  const [opacity, setOpacity] = useState (.5)
+
+
+  return  (
     <div className="main-container">
+      <div id="box-container">
+        <div id="box" 
+        style={{
+          width: width,
+          height: height,
+          transform:  'rotate(' + rotation + 'deg)',
+          borderRadius: border,
+          opacity: opacity
+        }}
+        
+        ></div>
+      </div>
       <div id="options-container">
         <div className="style-container">
           <h4>Dimmensions</h4>
           <p>Width</p>
-          <button>+</button>
-          <button>-</button>
-          <p>Hieght</p>
-          <button>+</button>
-          <button>-</button>
+            <input type="range"
+            min='0'
+            max='200'
+            step='2'
+            value={width}
+            onChange={(event) => {
+              setWidth(Number(event.target.value))
+            }}
+            />
+          <p>Height</p>
+            <input type="range"
+            min='2'
+            max='200'
+            step='2'
+            value={height}
+            onChange={(event) => {
+              setHeight(Number(event.target.value))
+            }}
+            />
         </div>
-        <Divider/>
+        <Divider orientation='vertical' style={{width: 1, height: 250, marginTop: 12}}/>
         <div className="style-container">
           <h4>Rotation</h4>
-          <p></p>
-          <button>+</button>
-          <button>-</button>
+            <input type="range"
+            min='0'
+            max='360'
+            step='2'
+            value={rotation}
+            onChange={(event) => {
+              setRotation(Number(event.target.value))
+            }}
+            />
         </div>
-        <Divider/>
+        <Divider orientation='vertical' style={{width: 1, height: 250, marginTop: 12}}/>
         <div className="style-container">
-          <h4>Color</h4>
-          <input type="range" name="set-color" id="set-color"/>
-          
+          <h4>Radius</h4>
+          <input type="range"
+          min='0'
+          max='100'
+          step='1'
+          value={border}
+          onChange={(event) => {
+            setBorder(Number(event.target.value))
+          }}
+          />
         </div>
-        <Divider/>
-        <div className="style-container">
-          <h4>Border</h4>
-          <p></p>
-          <button>+</button>
-          <button>-</button>
-        </div>
-        <Divider/>
-      </div>
+        <Divider orientation='vertical' style={{width: 1, height: 250, marginTop: 12}}/>
       <div className="style-container">
           <h4>Opacity</h4>
-          <p></p>
-          <button>+</button>
-          <button>-</button>
+          <input
+            type='range'
+            min='.1'
+            max='1'
+            step='.1'
+            defaultValue={opacity}
+            onChange={(event) => {
+              setOpacity(Number(event.target.value));
+            }}
+          />
         </div>
-        <Divider/>
+      </div>
     </div>
   );
 }
